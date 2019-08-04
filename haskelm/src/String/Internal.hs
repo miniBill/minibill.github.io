f42e5bee33ce791e19918c8a27c24db703b277ad
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2019-08-04 17:55:22 +0200

    Everything works with the new Prelude

--- haskelm/src/String/Internal.hs
+++ haskelm/src/String/Internal.hs
@@ -3,13 +3,17 @@ module String.Internal
   , unpack
   ) where
 
-import qualified Data.Text as T
-import           Kernel    (Char)
+import qualified Data.String
+import qualified Data.Text   as T
+import           Kernel      (Char)
 
 newtype String =
   String
     { runString :: T.Text
     }
+
+instance Data.String.IsString String where
+  fromString s = String (T.pack s)
 
 unpack :: String -> [Char]
 unpack (String s) = T.unpack s
